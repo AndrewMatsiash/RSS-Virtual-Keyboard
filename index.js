9eec60f7c08fdc494051a4944554e233df33e2b0
AndrewMatsiash <rabotatnadomne@gmail.com>
2022-05-07 12:46:07 +0300

    feat:added keys Tab CapsLock,Space,Enter,Backspace

--- index.js
+++ index.js
@@ -57,10 +57,12 @@ let keyboard = [{
 {
   "key": "Backspace",
   "code": "Backspace",
+  "class": "other-btn"
 },
 {
   "key": "Tab",
   "code": "Tab",
+  "class": "other-btn"
 },
 {
   "key": "q",
@@ -129,10 +131,12 @@ let keyboard = [{
 {
   "key": "Delete",
   "code": "Delete",
+  "class": "other-btn"
 },
 {
   "key": "CapsLock",
   "code": "CapsLock",
+  "class": "other-btn"
 },
 {
   "key": "a",
@@ -192,10 +196,12 @@ let keyboard = [{
 {
   "key": "Enter",
   "code": "Enter",
+  "class": "other-btn"
 },
 {
   "key": "Shift",
   "code": "ShiftLeft",
+  "class": "other-btn"
 },
 {
   "key": "z",
@@ -254,26 +260,32 @@ let keyboard = [{
 {
   "key": "Shift",
   "code": "ShiftRight",
+  "class": "other-btn"
 },
 {
   "key": "Control",
   "code": "ControlLeft",
+  "class": "other-btn"
 },
 {
   "key": "Meta",
   "code": "MetaLeft",
+  "class": "other-btn"
 },
 {
   "key": "Alt",
   "code": "AltLeft",
+  "class": "other-btn"
 },
 {
   "key": " ",
   "code": "Space",
+  "class": "other-btn"
 },
 {
   "key": "Alt",
   "code": "AltRight",
+  "class": "other-btn"
 },
 {
   "key": "←",
@@ -291,6 +303,7 @@ let keyboard = [{
 {
   "key": "Control",
   "code": "ControlRight",
+  "class": "other-btn"
 }
 ]
 
@@ -307,58 +320,224 @@ body.prepend(keyboardWrapper)
 
 
 //created element textarea class = 'use-keyboard-input'
-let textarea = document.createElement('textarea')
-textarea.classList.add('use-keyboard-input')
+let textArea = document.createElement('textarea')
+textArea.classList.add('use-keyboard-input')
+textArea.setAttribute("rows", 20)
+
 
 //created element div class = 'keyboard__body'
 let keyboardBody = document.createElement('div')
 keyboardBody.classList.add('keyboard__body')
 
 //added an element to the end of an element keyboardWrapper
-keyboardWrapper.append(textarea,keyboardBody)
+keyboardWrapper.append(textArea, keyboardBody)
 
 //function creates div class = 'k-key and keys.code' + data-atribute
 //added element to the keyboardBody
 function init() {
   let out = ''
   for (let i = 0; i < keyboard.length; i++) {
-    // let divKey = document.createElement('div')
-    // divKey.classList.add('k-key',`${keyboard[i].code}`)
-    // divKey.innerText = keyboard[i].key
-    // keyboardBody.appendChild(divKey)
-    out += `<div class = 'k-key ${keyboard[i].code}' data= ${keyboard[i].code}>${keyboard[i].key}</div>`
+    if (keyboard[i].class === "other-btn") {
+
+      out += `<div class = 'k-key ${keyboard[i].code} ${keyboard[i].class}' data= ${keyboard[i].code}>${keyboard[i].key}</div>`
+    } else {
+
+      out += `<div class = 'k-key ${keyboard[i].code} simvol_keys' data= ${keyboard[i].code}>${keyboard[i].key}</div>`
+
+    }
+
   }
   keyboardBody.innerHTML = out
 }
 
 init()
 
+
 // find all keys with a class
-let kKeys = document.querySelectorAll('.keyboard__body .k-key')
+let kKeysSimvol = document.querySelectorAll('.simvol_keys')
+let allKeys = document.querySelectorAll('.k-key')
+let otherKeys = document.querySelectorAll('.other-btn')
+let textAreaValue = textArea.value
 
 //I hang the listener on the document on pressing on the keyboard
-document.onkeydown = (event)=>{
+document.onkeydown = (event) => {
+  textArea.focus();
+
   console.log(event.code);
   document.querySelectorAll(`.k-key`).forEach(element => {
     element.classList.remove('active')
   });
-   document.querySelector(`.k-key[data =${event.code}]`).classList.add('active');
-  textarea.textContent += event.key
-  
+  document.querySelector(`.k-key[data =${event.code}]`).classList.add('active');
+  textArea.textContent += event.key
 }
 
+document.onkeyup = (event) => {
+  document.querySelectorAll(`.k-key`).forEach(element => {
+    element.classList.remove('active')
+  });
+}
+
+
+
+
 //I hang the listener on the keys on click mouse
-kKeys.forEach(element => {
-  element.addEventListener('click',(event)=>{
+allKeys.forEach(element => {
+  element.addEventListener('click', (event) => {
     console.log(event.target);
-  kKeys.forEach(element => {
+    allKeys.forEach(element => {
       element.classList.remove('active')
-      
     });
     event.target.classList.add('active')
-    textarea.textContent += event.target.innerText
   })
 });
 
+function pushSimvolTextarea() {
+  textArea.focus();
+  textArea.value += event.target.textContent
+}
+
+let flag = false
+kKeysSimvol.forEach(element => {
+  element.addEventListener('click', pushSimvolTextarea)
+});
+
+otherKeys.forEach(element => {
+  if (element.classList.contains('Backspace')) {
+    element.addEventListener('click', Backspace)
+  }
+  if (element.classList.contains('Space')) {
+    element.addEventListener('click', SpaceText)
+  }
+
+  if (element.classList.contains('Delete')) {
+    element.addEventListener('click', Delete)
+  }
+
+  if (element.classList.contains('Enter')) {
+    element.addEventListener('click', Enter)
+  }
+
+  if (element.classList.contains('Tab')) {
+    element.addEventListener('click', Tab)
+  }
+
+  if (element.classList.contains('CapsLock')) {
+    element.addEventListener('click',CapsLock )
+  }
+
+})
 
 
+
+
+
+
+
+
+
+
+
+
+function getCaret(el) {
+  if (el.selectionStart) {
+    return el.selectionStart;
+  } else if (document.selection) {
+    el.focus();
+
+    let r = document.selection.createRange();
+    if (r == null) {
+      return 0;
+    }
+
+    let re = el.createTextRange(),
+      rc = re.duplicate();
+    re.moveToBookmark(r.getBookmark());
+    rc.setEndPoint('EndToStart', re);
+
+    return rc.text.length;
+  }
+  return 0;
+}
+
+function resetCursor(txtElement, currentPos) {
+  if (txtElement.setSelectionRange) {
+    txtElement.focus();
+    txtElement.setSelectionRange(currentPos, currentPos);
+  } else if (txtElement.createTextRange) {
+    var range = txtElement.createTextRange();
+    range.moveStart('character', currentPos);
+    range.select();
+  }
+}
+
+
+function Backspace() {
+  let currentPos = getCaret(textArea);
+  console.log(getCaret(textArea));
+  let text = textArea.value;
+
+  let backSpace = text.substr(0, currentPos - 1) + text.substr(currentPos, text.length);
+
+  textArea.value = backSpace;
+
+  resetCursor(textArea, currentPos - 1);
+}
+
+function SpaceText() {
+  let currentPos = getCaret(textArea);
+  console.log(getCaret(textArea));
+  let text = textArea.value
+  let space = ' ';
+  let Space = text + space
+  textArea.value = Space;
+  resetCursor(textArea, currentPos + 1);
+}
+
+function Delete() {
+  let currentPos = getCaret(textArea);
+  console.log(getCaret(textArea));
+  let text = textArea.value;
+  let backSpace = text.substr(0, currentPos) + text.substr(currentPos + 1, text.length);
+  textArea.value = backSpace;
+  resetCursor(textArea, currentPos);
+
+}
+
+function Enter() {
+  textArea.focus()
+  let currentPos = getCaret(textArea);
+  let text = textArea.value;
+
+  if (currentPos === text.length) {
+    textArea.value += "\r\n";
+  }
+  if (currentPos < text.length) {
+    text2 = (text.substr(0, currentPos - 1) + "\r\n") + text.substr(currentPos, text.length)
+    textArea.value = text2;
+    resetCursor(textArea, currentPos);
+  }
+}
+
+function Tab() {
+  let currentPos = getCaret(textArea);
+  console.log(getCaret(textArea));
+  let text = textArea.value
+  let tabSimvol = '\t';
+  let Tab = text + tabSimvol
+  textArea.value = Tab;
+  resetCursor(textArea, currentPos + 1);
+}
+
+function CapsLock() {
+    if (flag == false) {
+      kKeysSimvol.forEach((el) => {
+        el.textContent = el.textContent.toUpperCase();
+        flag = true;
+      })
+    } else {
+      kKeysSimvol.forEach((el) => {
+        el.textContent = el.textContent.toLowerCase();
+        flag = false;
+      })
+    }
+  }
